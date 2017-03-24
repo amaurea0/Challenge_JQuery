4a058c2441f0a0f26ccd6a10ca91ab17db68897e
Amaurea <aurore.chatagnier@gmail.com>
2017-03-24 16:38:40 +0100

    Base model for carousel

---
+++ carousel.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    var $carrousel = $('#carrousel'),
+        $img = $('#carrousel img'),
+        indexImg = $img.length - 1, // last element index
+        i = 0; // comptor
+
+    function showImg() {
+        $img.css('display', 'none');
+        $currentImg = $img.eq(i);   //display current picture
+        $currentImg.css('display', 'block');
+    }
+
+    showImg();
+
+    $('.next').click(function () { // next picture
+        i++; 
+        if (i > indexImg) i = 0;
+        showImg();
+
+    });
+
+    $('.prev').click(function () { // previous picture
+        i--;
+        if (i > indexImg) i = indexImg;
+        showImg();
+    });
+
+    function slideImg() {
+        setInterval(function () {
+            i++;
+            if (i > indexImg) i = 0;
+            showImg();
+        }, 5000); // interval 5000 millisecondes (5s)
+    }
+
+    slideImg();
+
+});
